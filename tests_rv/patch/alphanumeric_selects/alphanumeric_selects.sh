767687bd21d63396154b838efc78a25fa47ffe6d
Conor Dooley <conor.dooley@microchip.com>
2023-02-09 08:47:43 +0000

    rv: make the alphanumeric select test report what it complained about
    Qinglin pointed out that the output from this test really isn't helpful
    at all as it doesn't say *what* it dislikes.
    This happens because the diff goes to stdout, but nipa only collects
    stderr. Redirect the output there instead.
    
    That said, the output isn't very useful.
    Apply some formatting to the output so that it suggests the fixup that
    it wants to see.
    
    Reported-by: Qinglin Pan <panqinglin2020@iscas.ac.cn>
    Signed-off-by: Conor Dooley <conor.dooley@microchip.com>

--- tests_rv/patch/alphanumeric_selects/alphanumeric_selects.sh
+++ tests_rv/patch/alphanumeric_selects/alphanumeric_selects.sh
@@ -11,21 +11,23 @@ HEAD=$(git rev-parse HEAD)
 
 git checkout -q HEAD~
 
-../../tests_rv/patch/alphanumeric_selects/alphanumeric_selects.pl arch/riscv/Kconfig > $tmpfile_b
+output_b=$(../../tests_rv/patch/alphanumeric_selects/alphanumeric_selects.pl arch/riscv/Kconfig)
 
-before=$(diff -y  --suppress-common-lines $tmpfile_b arch/riscv/Kconfig | wc -l)
+diff -y --suppress-common-lines <(echo "$output_b") arch/riscv/Kconfig > $tmpfile_b
+before=$(wc -l < $tmpfile_b)
 
 git checkout -q $HEAD
 
-../../tests_rv/patch/alphanumeric_selects/alphanumeric_selects.pl arch/riscv/Kconfig > $tmpfile_n
+output_n=$(../../tests_rv/patch/alphanumeric_selects/alphanumeric_selects.pl arch/riscv/Kconfig)
 
-now=$(diff -y  --suppress-common-lines $tmpfile_n arch/riscv/Kconfig | wc -l)
+diff -y --suppress-common-lines <(echo "$output_n") arch/riscv/Kconfig > $tmpfile_n
+now=$(wc -l < $tmpfile_n)
 
 echo "Out of order selects before the patch: $before and now $now" >&$DESC_FD
 
 if [ $now -gt $before ]; then
   echo "New out of order selects added" 1>&2
-  diff -U 0 $tmpfile_b $tmpfile_n
+  diff -U0 <(echo "$output_b") <(echo "$output_n")
 else
   rc=0
 fi
