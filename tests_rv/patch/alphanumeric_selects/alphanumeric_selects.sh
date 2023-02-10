12702f35e6552d1b9b65aadffe78eedcaa085636
Conor Dooley <conor.dooley@microchip.com>
2023-02-10 12:30:41 +0000

    fixup! rv: make the alphanumeric select test report what it complained about
    Signed-off-by: Conor Dooley <conor.dooley@microchip.com>

--- tests_rv/patch/alphanumeric_selects/alphanumeric_selects.sh
+++ tests_rv/patch/alphanumeric_selects/alphanumeric_selects.sh
@@ -13,21 +13,23 @@ git checkout -q HEAD~
 
 output_b=$(../../tests_rv/patch/alphanumeric_selects/alphanumeric_selects.pl arch/riscv/Kconfig)
 
-diff -y --suppress-common-lines <(echo "$output_b") arch/riscv/Kconfig > $tmpfile_b
+diff -y <(echo "$output_b") arch/riscv/Kconfig > $tmpfile_b
 before=$(wc -l < $tmpfile_b)
 
 git checkout -q $HEAD
 
 output_n=$(../../tests_rv/patch/alphanumeric_selects/alphanumeric_selects.pl arch/riscv/Kconfig)
 
-diff -y --suppress-common-lines <(echo "$output_n") arch/riscv/Kconfig > $tmpfile_n
+diff -y <(echo "$output_n") arch/riscv/Kconfig > $tmpfile_n
 now=$(wc -l < $tmpfile_n)
 
 echo "Out of order selects before the patch: $before and now $now" >&$DESC_FD
 
 if [ $now -gt $before ]; then
-  echo "New out of order selects added" 1>&2
-  diff -U0 <(echo "$output_b") <(echo "$output_n")
+  echo "New out of order content added" 1>&2
+  echo "Ideally, sort depends first, the default second, selects (in alphanumerical order) third." 1>&2
+  echo "Suggested fixups:" 1>&2
+  diff -U0 $tmpfile_b $tmpfile_n | tail -n +4 | cut -c 2- 1>&2
 else
   rc=0
 fi
